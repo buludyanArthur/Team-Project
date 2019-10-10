ef3e255b05f1149f47da5d4ed19531e44ce83aa7
Bella <bellak1985@gmail.com>
2019-10-10 20:47:17 +0400

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAO6qJ3EFwxTiLocKCWI6HZa19zINP4ozI",
+    authDomain: "book-store-optimizers.firebaseapp.com",
+    databaseURL: "https://book-store-optimizers.firebaseio.com",
+    projectId: "book-store-optimizers",
+    storageBucket: "book-store-optimizers.appspot.com",
+    messagingSenderId: "890674273077",
+    appId: "1:890674273077:web:ffd21f3c1308b0796c0a7c",
+    measurementId: "G-Y9LCDS55DP"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  export default firebase;
