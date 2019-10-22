4d74c2444216053db7ab11ee8330f362238e4857
Rob Tonoyan <Rob_Tonoyan@mail.ru>
2019-10-22 16:34:20 +0400

    initial menu

---
+++ src/books/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Logo(){
+    return(
+        <div className='logo'>
+            <div><img src=''  alt='logo'/></div>
+            <div className='logo-child'>
+            <div><i className="fas fa-cart-arrow-down fa-2x"></i></div>
+            <div><span>|</span></div>
+            <div><p>Մուտք</p></div>
+            </div>
+        </div>
+    )
+}
+export default Logo;
