51b2117cf3875e91b6d6e955c8e476748bb0dcfd
sweetRob <Rob_Tonoyan@mail.ru>
2019-10-24 19:39:31 +0400

    Add about us, header section and index.js

--- src/books/AboutUs.js
+++ src/books/AboutUs.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 export default function AboutUs () {
     return (
-        <div>
-            <p>AboutUs</p>
+        <div className='AboutUs'>
+            <p><h1>About us</h1></p>
+                <h4>
+                    Monday - Saturday 10:00am - 7:00 pm
+                <h4>
+                    Closed on Sundays
+                </h4>
+                </h4>
+                <h4>
+                    
+                </h4>
         </div>
     )
 }
--- src/books/Header.js
+++ src/books/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar,NavDropdown,FormControl,Form,Button,Nav} from 'react-bootstrap';
+import {Navbar,NavDropdown,FormControl,Form,Nav} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 
@@ -13,16 +13,16 @@ function Header () {
                 <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
                 <Nav.Link as={Link}  to='/aboutus'>About Us</Nav.Link>
                 <NavDropdown title="Book" className='header-child-2' id="basic-nav-dropdown">
-                    <NavDropdown.Item as={Link} to='action/3.1'>Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to='action/3.1'>BELLES-LETTRES</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">BELLES-LETTRESTEXTBOOKS</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">CHILDREN'S</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">NON-FICTIONIT </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.5"> IT and PROGRAMMING </NavDropdown.Item>
                 </NavDropdown>
                 </Nav>
   </Navbar.Collapse>
   <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" >
-
       </FormControl>
     </Form>
 </Navbar>
